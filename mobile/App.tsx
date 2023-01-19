d571f9a4cb4eaebb05b9179308740ac0984c8e83
dreialcantara <andreialcantara@id.uff.br>
2023-01-18 21:30:27 -0300

    remove submodules

---
+++ mobile/App.tsx
@@ -0,0 +1,36 @@
+import { StatusBar } from "react-native";
+import './src/lib/dayjs';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+} from "@expo-google-fonts/inter";
+
+import { Loading } from "./src/components/Loading";
+import { Home } from "./src/screens/Home";
+
+export default function App() {
+  const [fonstLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
+  });
+
+  if (!fonstLoaded) {
+    return <Loading />;
+  }
+
+  return (
+    <>
+      <Home />
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor={"transparent"}
+        translucent
+      />
+    </>
+  );
+}

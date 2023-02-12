cfaee91182b0df802a89cec2f3b3219342a7550a
dreialcantara <andreialcantara@id.uff.br>
2023-02-12 10:55:48 -0300

    fix

--- src/components/Habit.tsx
+++ src/components/Habit.tsx
@@ -7,3 +7,5 @@ interface HabitProps {
 export function Habit(props: HabitProps) {
   return <div className='bg-zinc-900 w-10 h-10 text-white rounded m-2 flex items-center justify-center'><p>{props.completed}</p></div>;
 }
+
+//
